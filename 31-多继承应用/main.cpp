bdea73ff0e6392f295517efb83cbae91491d9cf4
Fearless <489368600@qq.com>
2021-05-11 16:29:01 +0800

    多继承应用

---
+++ 31-多继承应用/main.cpp
@@ -0,0 +1,50 @@
+//
+//  main.cpp
+//  31-多继承应用
+//
+//  Created by XFB on 2021/5/11.
+//
+
+#include <iostream>
+
+// 教师
+class Teacher {
+public:
+    virtual void playBasketball() = 0;
+    virtual void playBaseBall() = 0;
+    virtual void playFootball() = 0;
+};
+
+// 保姆
+class BabySitter() {
+public:
+    virtual void clean() = 0; // 打扫
+    virtual void cook() = 0; // 烹饪
+};
+
+class Student: public Teacher, public Worker {
+    int m_score;
+public:
+    void playBasketball() {}
+    void playBaseBall() {}
+    void playFootball() {}
+    void clean() {}
+    void cook() {}
+};
+
+class Worker : public Teacher {
+    int m_salary;
+public:
+    void playBasketball() {}
+    void playBaseBall() {}
+    void playFootball() {}
+};
+
+
+int main() {
+
+    
+    
+    getchar();
+    return 0;
+}
