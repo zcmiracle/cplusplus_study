63a4481f2aa61ba623bdd1e7e6ac857fcf4e6a11
Fearless <489368600@qq.com>
2021-05-13 18:08:44 +0800

    赋值运算符某段出问题

---
+++ 44-赋值运算符注意/main.cpp
@@ -0,0 +1,42 @@
+//
+//  main.cpp
+//  44-赋值运算符注意
+//
+//  Created by XFB on 2021/5/13.
+//
+
+#include <iostream>
+using namespace std;
+
+class Person {
+public:
+    int m_age;
+        
+    Person &operator=(const Person &person) {
+        m_age = person.m_age;
+    }
+};
+
+class Student: public Person {
+public:
+    int m_score;
+    Student &operator=(const Student &student) {
+        // 父类的m_age不能调用，调用父类的重载函数给m_age赋值
+        // 类似于拷贝构造函数
+        Person::operator=(student);
+        m_score = student.m_score;
+    }
+};
+
+int main() {
+
+    Student stu1;
+    stu1.m_age = 20;
+    stu1.m_score = 100;
+    
+    Student stu2;
+    stu2 = stu1;
+    
+    getchar();
+    return 0;
+}
