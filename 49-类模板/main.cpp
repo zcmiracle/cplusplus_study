b388d10af28781d4cf52a4fcd21c072de07a6dd9
Fearless <489368600@qq.com>
2021-05-14 18:20:29 +0800

    模板有些问题

---
+++ 49-类模板/main.cpp
@@ -0,0 +1,33 @@
+//
+//  main.cpp
+//  49-类模板
+//
+//  Created by XFB on 2021/5/14.
+//
+
+#include <iostream>
+#include "Array.hpp"
+
+int main() {
+
+    Array<int> array(5);
+//    array.add(10);
+//    array.add(20);
+//    array.add(30);
+//    array.add(40);
+//    array.add(50);
+
+
+    cout << array << endl;
+    
+//    std::cout << array.get(0) << std::endl;
+//    std::cout << array.size() << std::endl;
+//    // 运算符重载
+//    std::cout << array[0] << std::endl;
+//    std::cout << array[2] << std::endl;
+//    std::cout << array[3] << std::endl;
+//    std::cout << array[4] << std::endl;
+
+    getchar();
+    return 0;
+}
