b81b66fa344d176b5e1b1a0c13e15db61efa8656
Fearless <489368600@qq.com>
2021-05-15 10:13:36 +0800

    类模板

--- 49-类模板/Array.hpp
+++ 49-类模板/Array.hpp
@@ -13,25 +13,45 @@
 using namespace std;
 
 template <typename Item>
+//No candidate function template was found for dependent friend function template specialization
+
 class Array {
-    
-    friend ostream &operator<<(ostream &, const Array<Item> &);
-    
+    // 友元函数
+//    friend ostream &operator<<<>(ostream&, const Array<Item>&);
     // 用于指向首元素
     Item *m_data;
     // 元素个数 初始化为0
     int m_size = 0;
     // 容量
     int m_capacity;
-
+    // 检查索引是否正确
+    void checkIndex(int index);
+    
 public:
     Array(int capacity = 0); // 构造函数
+    Array(const Array &array); // 拷贝构造函数
     ~Array(); // 析构函数
+    void setData(int index, Item value); // 赋值
     void add(Item value); // 添加
+    void remove(int index); // 移除
+    void insert(int index, Item value); // 插入
     Item get(int index); // 获取索引值
     int size();
+    
+    // 此处有点点问题
     Item operator[](int index); // 运算符重载[]
 };
+
+template <typename Item>
+void Array<Item>::checkIndex(int index) {
+//    if (index < 0 || index >= m_size) {
+//        throw  "数组下标越界";
+//    }
+    if (index < 0 || index >= m_size) {
+        cout << "\nError index = " << index << " out_of_range" <<endl;
+        exit(1);
+    }
+}
 
 // 构造函数
 template <typename Item>
@@ -41,11 +61,31 @@ Array<Item>::Array(int capacity) {
     m_data = new Item[capacity];
 }
 
+// 拷贝构造函数
+template <typename Item>
+Array<Item>::Array(const Array &array) {
+    m_size = array.m_size;
+    m_capacity = new int[array.m_size];
+    for (int i = 0; i < array.m_size; i ++) {
+        m_data[i] = array.m_data[i];
+    }
+}
+
 // 析构函数
 template <typename Item>
 Array<Item>::~Array() {
     if (m_data == NULL) return;
     delete[] m_data;
+}
+
+// 赋值
+template <typename Item>
+void Array<Item>::setData(int index, Item value) {
+    if (index < 0 || index >= m_size) {
+        // 抛出异常
+        throw "数组下标越界";
+    }
+    m_data[index] = value;
 }
 
 // 添加
@@ -61,13 +101,24 @@ void Array<Item>::add(Item value) {
     m_data[m_size++] = value;
 }
 
+template <typename Item>
+void Array<Item>::remove(int index) {
+    checkIndex(index);
+}
+
+template <typename Item>
+void Array<Item>::insert(int index, Item value) {
+    
+}
+
 // 获取索引值
 template <typename Item>
 Item Array<Item>::get(int index) {
-    if (index < 0 || index >= m_size) {
-        // 抛出异常
-        throw "数组下标越界";
-    }
+//    if (index < 0 || index >= m_size) {
+//        // 抛出异常
+//        throw "数组下标越界";
+//    }
+    checkIndex(index);
     return m_data[index];
 }
 
@@ -78,21 +129,25 @@ int Array<Item>::size() {
 
 template <typename Item>
 Item Array<Item>::operator[](int index) {
+    if (index < 0 || index >= m_size) {
+        cout << "\nError index = " << index << " out_of_range" <<endl;
+        exit(1);
+    }
     return get(index);
 }
 
-template <typename Item>
-// 友元函数有泛型 加上 <>
-ostream &operator<<(ostream &cout, const Array<Item> &array) {
-    cout << "[";
-    
-    for (int i = 0; i < array.m_size; i ++) {
-        if (i != 0) {
-            cout << ",";
-        }
-        cout << array.m_data[i];
-    }
-    return cout << "]";
-}
+//template <typename Item>
+//// 友元函数有泛型 加上 <>
+//ostream &operator<<<>(ostream& cout, const Array<Item>& array) {
+//    cout << "[";
+//    for (int i = 0; i < array.m_size; i ++) {
+//        if (i != 0) {
+//            cout << ",";
+//        }
+//        cout << array.m_data[i];
+//    }
+//    return cout << "]";
+//}
+
 
 #endif /* Array_hpp */
--- 49-类模板/main.cpp
+++ 49-类模板/main.cpp
@@ -7,26 +7,32 @@
 
 #include <iostream>
 #include "Array.hpp"
+using namespace std;
 
 int main() {
 
     Array<int> array(5);
-//    array.add(10);
-//    array.add(20);
-//    array.add(30);
-//    array.add(40);
-//    array.add(50);
+    array.add(10);
+    array.add(20);
+    array.add(30);
+    array.add(40);
+    array.add(50);
 
+    cout << "array[0] = " << array[0] << endl;
+    array.setData(0, 2000);
+    cout << "array[0] = " << array[0] << endl;
 
-    cout << array << endl;
+    // 重点
+//    cout << array << endl;
     
-//    std::cout << array.get(0) << std::endl;
-//    std::cout << array.size() << std::endl;
-//    // 运算符重载
-//    std::cout << array[0] << std::endl;
-//    std::cout << array[2] << std::endl;
-//    std::cout << array[3] << std::endl;
-//    std::cout << array[4] << std::endl;
+    
+//    cout << array.get(0) << endl;
+//    cout << array.size() << endl;
+    
+    // 运算符重载
+    cout << "array[2] = " << array[2] << endl;
+    cout << "array[3] = " << array[3] << endl;
+    cout << "array[4] = " << array[4] << endl;
 
     getchar();
     return 0;
