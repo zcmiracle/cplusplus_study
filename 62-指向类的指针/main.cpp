497a7caae1789df93a5810d149fa643ed3fa8eba
Fearless <489368600@qq.com>
2021-06-23 15:34:35 +0800

    指向类的指针

---
+++ 62-指向类的指针/main.cpp
@@ -0,0 +1,59 @@
+//
+//  main.cpp
+//  62-指向类的指针
+//
+//  Created by XFB on 2021/6/23.
+//
+
+#include <iostream>
+using namespace std;
+
+
+// 矩形
+class Rectangle {
+
+public:
+    Rectangle(double width, double height); // 有参构造函数
+    double Area();                          // 矩形面积
+    int compare(Rectangle rectangle);       // 面积比较函数
+    
+private:
+    double Width;       // 矩形的宽度
+    double Height;      // 矩形的高度
+};
+
+// 有参构造函数
+Rectangle::Rectangle(double width, double height) {
+    cout << "我是一个有参构造函数" << endl;
+    Width = width;
+    Height = height;
+}
+
+// 矩形的面积
+double Rectangle::Area() {
+    return Width * Height;
+}
+
+// 矩形面积的比较
+int Rectangle::compare(Rectangle rectangle) {
+    double max;
+    max = this->Area() > rectangle.Area() ? this->Area() : rectangle.Area();
+    return max;
+}
+
+
+int main() {
+
+    Rectangle rect1(10, 20);
+    Rectangle rect2(15, 15);
+    Rectangle *R;               // 定义一个指针
+   
+    R = &rect1;
+    cout << "第一个矩形的面积是：" << R->Area() << endl;
+    
+    R = &rect2;
+    cout << "第二个矩形的面积是：" << R->Area() << endl;
+
+    getchar();
+    return 0;
+}
