308dbe901326ce980b8ca5e86283a00c3fbed4be
zcmiracle <489368600@qq.com>
2021-06-25 23:27:09 +0800

    公有保护类

--- 68-保护派生/main.cpp
+++ 68-保护派生/main.cpp
@@ -10,6 +10,53 @@
 using namespace std;
 
 
+class S {
+public:
+    S(int x, int y) {
+        a = x;
+        b = y;
+    }
+    
+    void print() {
+        cout << a << endl;
+        cout << b << endl;
+    }
+    
+    ~S() {
+        cout << "调用S析构函数" << endl;
+    }
+
+private:
+//    int a, b;
+protected:
+    int a, b;
+
+};
+
+class Z : public S {
+public:
+    
+    // 一定要加上 :S(x, y)
+    // Z是继承自S，是派生类，需要加上基类的构造函数
+    // 派生类中 不能定义基类的对象 **** 重点 ****
+    Z(int x, int y) : S(x, y) {
+        // a = x;  // 'a' is a private member of 'S'
+        // b = y;  // 'b' is a private member of 'S'
+    }
+    
+    ~Z() {
+        cout << "调用Z的析构函数" << endl;
+    }
+private:
+    
+};
+
+
+
+
+
+
+// ===========================================
 class A {
     
 public:
@@ -47,7 +94,14 @@ int main() {
 //    b.z;
     
     C c;
-//    c.print1();    
+//    c.print1();
+    
+    
+    S s1(1, 3);
+    s1.print();
+    
+    Z z1(3, 5);
+    z1.print();
     
     getchar();
     return 0;
