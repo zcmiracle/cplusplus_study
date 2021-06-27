88ff2202119713a229b4cdbae821eced42208714
zcmiracle <489368600@qq.com>
2021-06-27 21:52:24 +0800

    虚基类

---
+++ 74-虚基类/main.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  74-虚基类
+//
+//  Created by Fearless on 2021/6/27.
+//
+
+#include <iostream>
+using namespace std;
+
+
+class A {
+    
+};
+
+class B: public A {
+    
+};
+
+
+class C: public A {
+    
+};
+
+
+int main() {
+
+    return 0;
+}
